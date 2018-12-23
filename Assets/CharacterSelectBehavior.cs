338822dbbae0563d9fda00594cc8c56ed5e13f56
Vivian Lim <vivvnlim@gmail.com>
2018-12-22 17:35:15 -0800

    add tilting and text

--- Assets/CharacterSelectBehavior.cs
+++ Assets/CharacterSelectBehavior.cs
@@ -7,37 +7,38 @@ public class CharacterSelectBehavior : MonoBehaviour {
 
     public GameObject SelectItems;
     public GameObject SelectionRectangle;
+    public GameObject SelectedText;
     public int Speed = 100;
 
     private int selectedIndex = 1;
-    private Transform[] SelectItemChildren;
+    private CharacterSelectItem[] SelectItemChildren;
+
+    private Quaternion initialRotation;
 
 	// Use this for initialization
 	void Start () {
-        SelectItemChildren = new Transform[SelectItems.transform.childCount];
-
-        var i = 0;
-
-        foreach (var child in SelectItems.transform)
-        {
-            SelectItemChildren[i] = child as Transform;
-            i++;
-        }
+        SelectItemChildren = GetComponentsInChildren<CharacterSelectItem>();
+        initialRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var selectedItem = SelectItemChildren[selectedIndex];
+        CharacterSelectItem selectedItem = SelectItemChildren[selectedIndex];
         if (selectedItem != null)
         {
             //SelectionRectangle.transform.SetPositionAndRotation(selectedItem.transform.position, selectedItem.transform.rotation);
-            SelectionRectangle.transform.position = Vector3.Lerp(SelectionRectangle.transform.position, selectedItem.transform.position, Speed * Time.deltaTime);
+            SelectionRectangle.transform.position = Vector3.Lerp(SelectionRectangle.transform.position, selectedItem.gameObject.transform.position, Speed * Time.deltaTime);
+            SelectionRectangle.transform.localScale = Vector3.Lerp(SelectionRectangle.transform.localScale, selectedItem.gameObject.transform.localScale, Speed * Time.deltaTime);
+            SelectedText.GetComponent<TextMesh>().text = selectedItem.Name;
         }
 
         if (Input.anyKeyDown)
         {
             selectedIndex = (selectedIndex + 1) % SelectItemChildren.Length; 
         }
+
+        transform.rotation = initialRotation * Quaternion.Euler(20 * new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal")));
+
 		
 	}
 
